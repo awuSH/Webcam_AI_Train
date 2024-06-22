414d1a52d6175a0da89177a15a296f015cd9a4ad
awuSH <wuescher@me.com>
2024-06-22 15:14:11 +0200

    tests

--- test_import.py
+++
@@ -1,5 +0,0 @@
-from routers.fetch_cam import *
-url = "https://archive1.roundshot.com/53aa8d33466fa9.62099805/2024-02-09/10-00-00/2024-02-09-10-00-00_half.jpg"
-url = "https://archive1.roundshot.com/53aa8d33466fa9.62099805/2024-01-27/11-30-00/2024-01-27-11-30-00_half.jpg"
-url = "https://storage.roundshot.com/53aa8d33466fa9.62099805/2023-12-30/12-00-00/2023-12-30-12-00-00_half.jpg"
-fetch_webcam_image(url)
