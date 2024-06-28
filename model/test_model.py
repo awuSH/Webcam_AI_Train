b4f588c9ab7c744bf8f1850e212fd448d4107223
awuSH <wuescher@me.com>
2024-06-28 09:35:00 +0200

    added statists

--- model/test_model.py
+++ model/test_model.py
@@ -1,9 +1,13 @@
+import os
+import shutil
 from keras.models import load_model
 from tensorflow.keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img
 import numpy as np
-import os
 from sklearn.metrics import confusion_matrix, classification_report
+import pandas as pd
+
 network_path = '/Volumes/netzlaufwerk/88_Coding/00_Datasets/dataset_skipiste_crowded/testdata_brightness/'
+misclassified_dir = 'statistics/misclassified_images'
 
 def load_image(img_path, target_size=(150, 150)):
     """
@@ -20,6 +24,29 @@ def load_image(img_path, target_size=(150, 150)):
     img_array = img_to_array(img)
     img_array = img_array / 255.0  # Normalisierung der Pixelwerte auf den Bereich [0, 1]
     return img_array
+
+def clear_directory(directory):
+    """
+    Löscht alle Dateien in einem Verzeichnis.
+    
+    Args:
+        directory (str): Der Pfad des Verzeichnisses.
+    """
+    for filename in os.listdir(directory):
+        file_path = os.path.join(directory, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print(f'Fehler beim Löschen der Datei {file_path}. Grund: {e}')
+
+# Ordner für falsch klassifizierte Bilder leeren
+if not os.path.exists(misclassified_dir):
+    os.makedirs(misclassified_dir)
+else:
+    clear_directory(misclassified_dir)
 
 # Modell laden
 model = load_model('model/trained_model_skicount.h5')
@@ -43,18 +70,8 @@ true_classes = test_generator.classes
 cm = confusion_matrix(true_classes, predicted_classes)
 cr = classification_report(true_classes, predicted_classes, target_names=test_generator.class_indices.keys())
 
-# Vorhersagen für jedes Testbild machen
-for i in range(len(test_generator.filenames)):
-    img_path = test_generator.filepaths[i]
-    img_label = test_generator.labels[i]
-    img = load_image(img_path)  # Lade das Testbild
-    
-    # Vorhersage für das Bild machen
-    prediction = model.predict(np.expand_dims(img, axis=0))
-    predicted_class = np.argmax(prediction)
-    
 # Öffne eine Textdatei zum Schreiben
-with open('predictions.txt', 'w') as file:
+with open('statistics/predictions.txt', 'w') as file:
     # Schreibe die Überschrift
     file.write("Pfad, pred_Klasse, ist_Klasse, Vorhersage (wahr/falsch)\n")
     
@@ -73,11 +90,22 @@ with open('predictions.txt', 'w') as file:
         
         # Schreibe die Informationen in die Datei
         file.write(f"{img_path}, {predicted_class}, {img_label}, {is_correct}\n")
+        
+        # Kopiere falsch klassifizierte Bilder in das Verzeichnis
+        if is_correct == "falsch":
+            shutil.copy(img_path, misclassified_dir)
 
-print("Vorhersagen wurden in 'predictions.txt' gespeichert.")
-with open('test_report.txt','w') as file:
+print("Vorhersagen wurden in 'statistics/predictions.txt' gespeichert.")
+
+# Schreibe Klassifizierungsbericht und Verwirrungsmatrix in 'statistics/statistic.txt'
+with open('statistics/statistic.txt','w') as file:
     file.write("Klassifizierungsbericht:\n" + cr)
     file.write("\nVerwirrungsmatrix:\n" + str(cm))
+
+# Verwirrungsmatrix als Tabelle formatieren und in eine Datei speichern
+cm_df = pd.DataFrame(cm, index=test_generator.class_indices.keys(), columns=test_generator.class_indices.keys())
+cm_df.to_csv('statistics/confusion_matrix.csv', sep='\t')
+
 # Ausgabe der Metriken
-print("Verwirrungsmatrix:\n", cm)
+print("Verwirrungsmatrix:\n", cm_df)
 print("Klassifizierungsbericht:\n", cr)
